f45555c18ce7b645bac1965ed8dc3b51350c547d
mrivar <martin7557@gmail.com>
2020-04-12 04:06:27 +0200

    Create info bubble template

---
+++ components/atoms/InfoBubble.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="infobubble">
+    <span class="infobubble_title" v-if="title">{{ title }}</span>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoBubble",
+  props: {
+    title: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.infobubble {
+  border-radius: 3px 3px 3px 3px;
+  -moz-border-radius: 3px 3px 3px 3px;
+  -webkit-border-radius: 3px 3px 3px 3px;
+  border: 0px solid #000000;
+  background-color: #FFF;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  & .infobubble_title {
+    position: absolute;
+    top: 8px;
+    left: 10px;
+  }
+}
+
+</style>
---
+++ components/atoms/InfoBubbleNumber.vue
@@ -0,0 +1,43 @@
+<template>
+  <InfoBubble :title="title">
+    <span class="value">{{ value }}</span>
+    <span class="note" v-if="note">{{ note }}</span>
+  </InfoBubble>
+</template>
+
+<script>
+import InfoBubble from '~/components/atoms/InfoBubble.vue';
+
+export default {
+  name: "InfoBubbleNumber",
+  components: {
+    InfoBubble
+  },
+  props: {
+    title: {
+      type: String
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: "blue"
+    },
+    note: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+.value {
+  font-size: 2.5rem;
+}
+
+.note {
+
+}
+</style>
