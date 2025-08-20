396a8c7c05761495e87e67b4b71a6ce46c24af10
mrivar <martin7557@gmail.com>
2025-08-20 23:31:15 +0200

    cv: add skeleton while mounting components

---
+++ components/blocks/Skeleton.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="skeleton-title bone"></div>
+  <div class="skeleton-container">
+    <div class="bone" style="width: 80px"></div>
+    <div class="bone" style="width: 40px"></div>
+    <div class="bone" style="width: 120px"></div>
+    <div class="bone" style="width: 90px"></div>
+    <div class="bone" style="width: 30px"></div>
+    <div class="bone" style="width: 50px"></div>
+    <div class="bone" style="width: 100px"></div>
+    <div class="bone" style="width: 50px"></div>
+    <div class="bone" style="width: 30px"></div>
+    <div class="bone" style="width: 130px"></div>
+    <div class="bone" style="width: 80px"></div>
+    <div class="bone" style="width: 60px"></div>
+  </div>
+  <div class="skeleton-container">
+    <div class="bone bone-2" style="width: 80px"></div>
+    <div class="bone bone-2" style="width: 40px"></div>
+    <div class="bone bone-2" style="width: 120px"></div>
+    <div class="bone bone-2" style="width: 90px"></div>
+    <div class="bone bone-2" style="width: 30px"></div>
+    <div class="bone bone-2" style="width: 50px"></div>
+    <div class="bone bone-2" style="width: 100px"></div>
+    <div class="bone bone-2" style="width: 50px"></div>
+    <div class="bone bone-2" style="width: 30px"></div>
+    <div class="bone bone-2" style="width: 130px"></div>
+    <div class="bone bone-2" style="width: 80px"></div>
+    <div class="bone bone-2" style="width: 60px"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.skeleton-title {
+  margin-top: 10px;
+  min-width: 180px;
+  margin-bottom: 24px;
+}
+
+.skeleton-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  text-align: left;
+  margin-bottom: 24px;
+}
+
+.bone {
+  width: 50px;
+  height: 40px;
+  border-radius: 5px;
+  background-color: #DDD;//#D6D9E0;
+}
+
+html.dark-mode {
+  & .bone {
+    background-color: #353A46;
+  }
+}
+</style>
--- pages/cv/index.vue
+++ pages/cv/index.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="cv-container">
-    <CvBlock title="about me">
-      I’m a Developer with a Master’s in Artificial Intelligence, with 9+ years of experience in building
-      scalable backend systems and ML solutions. Passionate about clean code, distributed
-      systems, and meaningful tech.<br><br>
-      What I value in a job opportunity is <b class="b">how interesting the project/s is</b> (technologically and/or
-      socially), a <b class="b">good working environment</b> (knowledge sharing, teamwork) and a <b class="b">modern company culture</b>
-      (remote work, flexible hours, personal development, etc.).
-    </CvBlock>
-    <CvSkills />
-    <CvExperience />
-    <CvEducation />
-    <CvPublications />
+    <template v-if="!mounted" class="text">
+      <Skeleton />
+    </template>
+
+    <template v-else>
+      <CvBlock title="about me">
+        I’m a Developer with a Master’s in Artificial Intelligence, with 9+ years of experience in building
+        scalable backend systems and ML solutions. Passionate about clean code, distributed
+        systems, and meaningful tech.<br><br>
+        What I value in a job opportunity is <b class="b">how interesting the project/s is</b> (technologically and/or
+        socially), a <b class="b">good working environment</b> (knowledge sharing, teamwork) and a <b class="b">modern company culture</b>
+        (remote work, flexible hours, personal development, etc.).
+      </CvBlock>
+      <CvSkills />
+      <CvExperience />
+      <CvEducation />
+      <CvPublications />
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+let mounted = ref<Boolean>(false)
+
+onMounted(() => {
+  mounted.value = true
+})
 </script>
 
 <style scoped lang="scss">
