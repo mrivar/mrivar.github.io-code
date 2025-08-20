29773b48b118bbc4036be56763b4cf43dfb92d51
mrivar <martin7557@gmail.com>
2025-08-20 17:35:08 +0200

    Skills: responsive

--- components/cv/blocks/CvSkills.vue
+++ components/cv/blocks/CvSkills.vue
@@ -1,10 +1,10 @@
 <template>
   <CvBlock title="skills">
     <div class="skills" ref="skills">
-      <div class="skills-container">
-        <CvSkillsBlock title="frontend" :items="frontend"/>
-        <CvSkillsBlock title="backend" :items="backend"/>
-        <CvSkillsBlock title="others" :items="others"/>
+      <div class="skills-grid">
+        <CvSkillsBlock title="frontend" :items="frontend" />
+        <CvSkillsBlock title="backend" :items="backend" />
+        <CvSkillsBlock title="others" :items="others" />
       </div>
     </div>
     <div class="skills-filler" ref="skills-filler"></div>
@@ -14,9 +14,15 @@
 <script setup lang="ts">
 import { useTemplateRef } from 'vue'
 
-const frontend = ["vue", "nuxt", "angular", "react", "typescript", "javascript"]
-const backend = ["kotlin", "python", "docker", "k8s", "microservices", "ci/cd"]
-const others = ["clean code", "solid principles", "problem solving", "critical thinking", "communication"]
+const frontend = ['vue', 'nuxt', 'angular', 'react', 'typescript', 'javascript']
+const backend = ['kotlin', 'python', 'docker', 'k8s', 'microservices', 'ci/cd']
+const others = [
+  'clean code',
+  'solid principles',
+  'problem solving',
+  'critical thinking',
+  'communication'
+]
 
 const skills = useTemplateRef<HTMLInputElement>('skills')
 const skillsFiller = useTemplateRef<HTMLInputElement>('skills-filler')
@@ -44,21 +50,31 @@ watchEffect(() => {
   padding: 48px;
   margin-top: 24px;
   background-color: $primary-color;
-  box-shadow: inset 0 20px 25px rgba(0, 0, 0, 0.1), inset 0px -20px 25px rgba(0, 0, 0, 0.1),
-  inset 0 10px 10px rgba(0, 0, 0, 0.04), inset 0px -10px 10px rgba(0, 0, 0, 0.04)
+  box-shadow: inset 0 20px 25px rgba(0, 0, 0, 0.1),
+    inset 0px -20px 25px rgba(0, 0, 0, 0.1),
+    inset 0 10px 10px rgba(0, 0, 0, 0.04),
+    inset 0px -10px 10px rgba(0, 0, 0, 0.04);
 }
 
 .skills-filler {
   margin-top: 24px;
 }
 
-.skills-container {
+.skills-grid {
   // griding
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   column-gap: 32px;
   // others
   max-width: 1100px;
-  margin: auto
+  margin: auto;
+}
+
+@include phone {
+  .skills-grid {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    row-gap: 32px;
+  }
 }
 </style>
